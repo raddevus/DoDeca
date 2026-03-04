f18d598c55259ced5918854757bf1883889e580d
raddevus <1radeutsch1@gmail.com>
2026-03-03 23:05:32 -0500

    initial creation of project & repo

---
+++ DoDeca.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace DoDeca.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ DoDeca/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DoDeca.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
