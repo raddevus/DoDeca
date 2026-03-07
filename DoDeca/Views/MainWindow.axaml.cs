f9b2eda5d37f5679b507c8697f08ead991e7f550
raddevus <1radeutsch1@gmail.com>
2026-03-07 15:03:36 -0500

    Changing from the SplitView to a Grid with GridSplitter so I can let user set the size.

--- DoDeca/Views/MainWindow.axaml.cs
+++ DoDeca/Views/MainWindow.axaml.cs
@@ -12,26 +12,37 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-   private async void MakeBigger(object? sender, RoutedEventArgs e){
-      if (isCtrlDown){ OpenPane.OpenPaneLength -= 10; return;}
-      OpenPane.OpenPaneLength += 10;
-   }
+/*   private async void MakeBigger(object? sender, RoutedEventArgs e){
+      if (isCtrlDown){ OpenNavPane.OpenNavPaneLength -= 10; return;}
+      OpenNavPane.OpenNavPaneLength += 10;
+   } */
    bool isCtrlDown = false;
    private void Window_KeyDown(object? sender, KeyEventArgs e)
    {
-      Console.WriteLine($"e.Key: {e.Key}");
+/*      Console.WriteLine($"e.Key: {e.Key}");
        if (e.Key == Key.LeftCtrl)
        {
           isCtrlDown = true;
           ChangeSize.Content = "Make Smaller";
            Console.WriteLine("Control key pressed");
-       }
+       }*/
    }
+private void OnSplitterDragDelta(object? sender, VectorEventArgs e)
+{
+    var newWidth = NavPane.Width + e.Vector.X;
 
+    if (newWidth <= NavPane.MinWidth)
+        newWidth = NavPane.MinWidth;
+
+    if (newWidth >= NavPane.MaxWidth)
+        newWidth = NavPane.MaxWidth;
+
+    NavPane.Width = 50; //newWidth;
+}
    private void Window_KeyUp(object? sender, KeyEventArgs e){
-      if (e.Key == Key.LeftCtrl){ 
+/*      if (e.Key == Key.LeftCtrl){ 
          isCtrlDown = false;
          ChangeSize.Content = "Make Bigger";
-      }
-   }
+      } */
+   } 
 }
