47ca2f69fa87c5e44a39fb65fcd88155f01ca0a5
raddevus <1radeutsch1@gmail.com>
2026-03-11 22:45:02 -0400

    Added IconSource to Node so we can display an icon no the treeview item

--- DoDeca/Models/Node.cs
+++ DoDeca/Models/Node.cs
@@ -7,6 +7,7 @@ public class Node{
     public string Name { get; set; }
     public StorageType StoreType { get; set; }
     public string Path {get;set;}
+    public string IconSource { get; set; } // Icon source for the tree view node
      public ObservableCollection<Node> Children { get; set; }
         = new ObservableCollection<Node>();
     public override string ToString(){
